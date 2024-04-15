628a7a09ad6dd91e011da94fe4d9058ae8392b3b
Alana Gabriele Amorim Silva <alanagabriele43@gmail.com>
2024-04-15 11:12:05 -0300

    Exercicio Count Visited Nodes

---
+++ CountVisitedNodesDirectedGraph/2876.CountVisitedNodesDirectedGraph.py
@@ -0,0 +1,33 @@
+class Solucao:
+    def countVisitedNodes(self, arestas):
+        n = len(arestas)
+
+        visitados = [0] * n
+        distancia = [0] * n
+        contador = [0] * n
+
+        for i in range(n):
+            if visitados[i] == 0:
+                self.dfs(arestas, i, visitados, distancia, contador)
+        
+        return contador
+
+    def dfs(self, arestas, i, visitados, distancia, contador):
+        visitados[i] = 1
+
+        proximo = arestas[i]
+        if visitados[proximo] == 0:
+            distancia[proximo] = distancia[i] + 1
+            contador[proximo] = contador[i] + 1
+            self.dfs(arestas, proximo, visitados, distancia, contador)
+
+            if distancia[proximo] < distancia[i] + 1:
+                contador[i] = contador[proximo]
+            else:
+                contador[i] = contador[proximo] + 1
+        elif visitados[proximo] == 1:
+            contador[i] = distancia[i] + 1 - distancia[proximo]
+        else:
+            contador[i] = contador[proximo] + 1
+
+        visitados[i] = 2
